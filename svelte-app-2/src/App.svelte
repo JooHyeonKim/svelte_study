ea4a49f35dcda75a32483c6180147ec1e6dfa444
JooHyeonKim <subin_282@naver.com>
2023-08-13 22:58:54 +0900

    test

---
+++ svelte-app-2/src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+	let arr = ['a','ab','abc','abcd','efg']
+
+	function deleteAlement(){
+		arr = arr.slice(1)
+	}
+</script>
+
+<ul>
+	{#each arr as ar}
+	<li>
+		{ar}
+	</li>
+	{/each}
+</ul>
+
+<button on:click={deleteAlement}>
+	Delete
+</button>
+
+<!-- <script>
+	let name = 'world';
+	let fruits = ['Apple','Banana','Cherry','Orange','Mango']
+
+	function deleteFruit(){
+		fruits = fruits.slice(1)
+	}
+</script>
+
+<h1>Hello {name}</h1>
+
+<ul>
+	
+	{#each fruits as fruit}
+		<li>{fruit}</li>
+	{/each}
+	
+</ul>
+
+<button on:click={deleteFruit}>
+	Eat it!
+</button> -->
---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+ import Hello from './Hello.svelte';
+	let name = 'world';
+</script>
+
+<h1>Hello {name}!</h1>
+
+<button on:click={() => { name = "Joohyeon"}}>
+Change!
+</button>
+
+<Hello></Hello>
+
+<style>
+h1{color: red;
+}
+</style>
