179102eeb89c5ba46c0a29d0477ad4dd90d0a477
Joohyeon <56497471+JooHyeonKim@users.noreply.github.com>
2023-08-29 15:32:20 +0900

    Create event_handling_practice.svelte

---
+++ svelte입문 가이드/event_handling_practice.svelte
@@ -0,0 +1,27 @@
+<script>
+	import {onMount} from 'svelte';
+	let name = 'world';
+	let message = "클릭하면 색깔이 변하는 마법의 박스"
+	let isPink = false
+	onMount(()=>{
+		const box = document.querySelector('.box')
+		box.addEventListener('click',()=>{isPink = !isPink;})
+	})
+</script>
+
+<h1>박스를 클릭해보세요</h1>
+
+<div class = "box"
+	   style = "background-color : {isPink? 'pink' : 'skyblue'};">
+	{message}
+</div>
+
+<style>
+	.box{
+		height: 300px;
+		width: 800px;
+		background-color : skyblue;
+	}
+	
+</style>
+
