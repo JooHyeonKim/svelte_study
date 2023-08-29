014028c3c148d8d730be7266f458eb3b13cebbca
Joohyeon <56497471+JooHyeonKim@users.noreply.github.com>
2023-08-29 23:53:16 +0900

    Create App.svelte

---
+++ svelte입문 가이드/component/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	let fruits = ['Apple', 'Banana', 'Cherry', 'Orange','Mango']
+</script>
+
+<h2>
+	Fruits
+</h2>
+
+<ul>
+	{#each fruits as f}
+	<li>
+		{f}
+	</li>
+	{/each}
+</ul>
+
+<h2>
+	Fruits reverse
+</h2>
+<ul>
+	{#each [...fruits].reverse() as f}
+	<li>
+		{f}
+	</li>
+	{/each}
+</ul>
+
+<h2>
+	Fruits slice -2
+</h2>
+
+<ul>
+	{#each fruits.slice(-2) as f}
+	<li>
+		{f}
+	</li>
+	{/each}
+</ul>
