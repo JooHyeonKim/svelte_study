f6952aa5b2eb611d1c85d265cd5286c1baa8d4fe
Joohyeon <56497471+JooHyeonKim@users.noreply.github.com>
2023-08-30 11:45:50 +0900

    Create Fruits.svelte

---
+++ svelte입문 가이드/component/Fruits.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let fruits
+	export let reverse
+	export let slice
+
+	let computedFruits = []
+	let title = ''
+
+	if(reverse){
+		computedFruits = [...fruits].reverse()
+		title = 'reverse'
+	} else if (slice) {
+		title = slice.split(',')
+		//... : 배열을 전개
+		//...slice.split(',') //split된 결과 배열요소들을 전개 ['0', '3'] => '0', '3'
+		computedFruits = fruits.slice(...slice.split(','))
+		title = `slice ${slice}`
+	}else{
+		computedFruits = [...fruits]
+	}
+
+</script>
+
+<h2> Fruits {title}</h2>
+
+<ul>
+	{#each computedFruits as fruit}
+	<li>{fruit}</li>
+	{/each}
+</ul>
